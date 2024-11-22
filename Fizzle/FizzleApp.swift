b0fe1bff582c2e4452bec41d7d1e3206652c87fe
Kyle Peterson <kdpeters@gmail.com>
2024-11-21 21:00:19 -0500

    Initial Commit

---
+++ Fizzle/FizzleApp.swift
@@ -0,0 +1,17 @@
+//
+//  FizzleApp.swift
+//  Fizzle
+//
+//  Created by Kyle Peterson on 11/21/24.
+//
+
+import SwiftUI
+
+@main
+struct FizzleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FizzleTests/FizzleTests.swift
@@ -0,0 +1,17 @@
+//
+//  FizzleTests.swift
+//  FizzleTests
+//
+//  Created by Kyle Peterson on 11/21/24.
+//
+
+import Testing
+@testable import Fizzle
+
+struct FizzleTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
